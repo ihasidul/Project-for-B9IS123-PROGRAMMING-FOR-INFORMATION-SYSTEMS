cade673fc455d01325e7d2835d46db6b9307df6c
ihasidul <ihasidul@gmail.com>
2025-06-11 06:18:22 +0100

    Update main

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from apps.product.routers import router as product_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # I will have to update this on production for security
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(product_router)
 
 @app.get("/")
 def health_check():
     return "Service is running!"
-
