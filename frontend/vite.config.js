0fc59ff21cffcaf279750760d4f5af2fbfd05ef4
ihasidul <ihasidul@gmail.com>
2025-06-11 09:12:14 +0100

    Update vite config to fix CORS issues while developing with different ip backend. Got help from this https://react-v9.holt.courses/lessons/core-react-concepts/jsx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {},
+  server: {
+    proxy: {
+      "/api": { target: "http://localhost:8000", changeOrigin: true },
+    },
+  },
   plugins: [react()],
 });
